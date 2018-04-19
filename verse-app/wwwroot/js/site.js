df0b1ae9d7196ae7f32500fe0d33b9b359e58df9
Stanley Tsao <stsao002@gmail.com>
2018-04-19 15:10:39 -0400

    Setup ESV API call

--- verse-app/wwwroot/js/site.js
+++ verse-app/wwwroot/js/site.js
@@ -1,1 +1,16 @@
 ﻿// Write your JavaScript code.
+
+var verse = "Proverbs" + Math.floor(Math.random() * 31) + ":" + Math.floor(Math.random() * 36);
+
+$(document).ready(function () {
+    $.ajax({
+        url: "https://api.esv.org/v3/passage/html/?q=" + verse + "&include-footnotes=false&include-footnote-body=false&include-audio-link=false&include-short-copyright=false",
+        type: 'GET',
+        headers: { "Authorization": "Token 00706264fcd4c12e299878dbdf3af0608adbabff" },
+        success: function (data) {
+            //alert(JSON.stringify(data));
+            //$("#Ref").text(JSON.stringify(data.canonical));
+            $("#Display").append(data.passages);
+        }
+    });
+});
