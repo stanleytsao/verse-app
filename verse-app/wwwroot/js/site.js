f704b7cfd8f83fb43b374218d0cde51f79c95de6
Stanley Tsao <stsao002@gmail.com>
2018-05-08 15:05:26 -0400

    Redesigned app to be verse typer

--- verse-app/wwwroot/js/site.js
+++ verse-app/wwwroot/js/site.js
@@ -1,32 +1,118 @@
-﻿var OldTestiment = ["Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua", "Judges", "Ruth", "1Samuel", "2Samuel", "1Kings", "2Kings", "1Chronicles", "2Chronicles", "Ezra", "Nehemiah", "Esther", "Job", "Psalms", "Proverbs", "Ecclesiastes", "SongofSolomon", "Isaiah", "Jeremiah", "Lamentations", "Ezekiel", "Daniel", "Hosea", "Joel", "Amos", "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk", "Zephaniah", "Haggai", "Zechariah", "Malachi"];
+﻿var Timer = null;
+var Time = 120;
+var LastTime = 120;
+var Score = 0;
+var Verse;
 
-var NewTestament = ["Matthew", "Mark", "Luke", "John", "Acts", "Romans", "1Corinthians", "2Corinthians", "Galatians", "Ephesians", "Philippians", "Colossians", "1Thessalonians", "2Thessalonians", "1Timothy", "2Timothy", "Titus", "Philemon", "Hebrews", "James", "1Peter", "2Peter", "1John", "2John", "3John", "Jude", "Revelation"];
+$(document).ready(function () {
+    $("#EndPage").hide();
+});
 
-var Test = ["John3:16", "Proverbs 3:5-6", "Romans 3:23", "Jeremiah 29:11"];
+$("#Start").click(function () {
+    $("#StartPage").hide();
+    GetVerse("jn3:16");
+    $("#Typed").focus();
+    setTimeout(
+        Timer = setInterval(TimeTick, 1000),
+        1000
+    );
+});
+
+$("#Submit").click(function () {
+    SubmitVerse();
+});
+
+$("#Typed").on("keypress", function (e) {
+    if (e.keyCode === 13) {
+        SubmitVerse();
+    }
+});
 
 function GetVerse(ref) {
     $.ajax({
-        url: "https://api.esv.org/v3/passage/text/?q=" + ref + "&include-passage-references=false&include-first-verse-numbers=false&include-verse-numbers=false&include-footnotes=false&include-footnote-body=false&include-short-copyright=true&include-passage-horizontal-lines=false&include-heading-horizontal-lines=false&include-headings=false",
+        url: "https://api.esv.org/v3/passage/html/?q=" + ref + "&include-verse-anchors=false&include-chapter-numbers=false&include-first-verse-numbers=false&include-footnotes=false&include-headings=false&include-subheadings=false&include-audio-link=false",
         type: 'GET',
         headers: { "Authorization": "Token 00706264fcd4c12e299878dbdf3af0608adbabff" },
         success: function (data) {
-            $("#Board").append('<div class="card"><div class="front">' + ref + '</div><div class="back">' + data.passages + '</div></div>');
-            $(".card").flip({
-                trigger: 'click'
-            });
+            $("#Verse").html(data.passages);
         }
     });
 }
 
-$(document).ready(function () {
-    //$.each(Test, function (key, value) {
-    //    GetVerse(value);
-    //});
-    while (Test.length > 0) {
-        var i = Math.floor(Math.random() * Test.length);
-        GetVerse(Test[i]);
-        $("#Board").append('<div class="card"><div class="front"></div><div class="back">' + Test[i] + '</div></div>');
-        Test.splice(i, 1);
+function SubmitVerse() {
+    var src = $("#Typed").val().replace(/\W/g, '');
+    var tgt = $(".woc").text().replace(/\W/g, '');
+    var max = calculateLevDistance("", tgt);
+    var act = calculateLevDistance(src, tgt);
+    var subscore = Math.round((1 - act / max) * max * 10 / (LastTime - Time));
+
+    Score += subscore;
+    $("#Score").text("Score: " + Score);
+    
+    var row = $("#History").insertRow(0);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    //cell1.innerHTML = subscore;
+    cell2.innerHTML = (1 - act / max).toFixed(2);
+    //cell3.innerHTML = LastTime - Time + " sec";
+    //cell4.innerHTML = $("#Typed").val();
+
+    LastTime = Time;
+    GetVerse();
+}
+
+function TimeTick() {
+    Time -= 1;
+    if (Time === 0) {
+        clearInterval(Timer);
+        $("#FinalScore").text("Score: " + Score);
+        $("#EndPage").show();
+    } else {
+        if (Time % 60 < 10) {
+            $("#Time").text("Time: " + Math.floor(Time / 60) + ":0" + Time % 60);
+        } else {
+            $("#Time").text("Time: " + Math.floor(Time / 60) + ":" + Time % 60);
+        }        
     }
-});
+}
+
+function calculateLevDistance(src, tgt) {
+    var realCost;
+
+    var srcLength = src.length,
+        tgtLength = tgt.length,
+        tempString, tempLength; // for swapping
+
+    var resultMatrix = new Array();
+    resultMatrix[0] = new Array(); // Multi dimensional
+
+    // To limit the space in minimum of source and target,
+    // we make sure that srcLength is greater than tgtLength
+    if (srcLength < tgtLength) {
+        tempString = src; src = tgt; tgt = tempString;
+        tempLength = srcLength; srcLength = tgtLength; tgtLength = tempLength;
+    }
+
+    for (var c = 0; c < tgtLength + 1; c++) {
+        resultMatrix[0][c] = c;
+    }
+
+    for (var i = 1; i < srcLength + 1; i++) {
+        resultMatrix[i] = new Array();
+        resultMatrix[i][0] = i;
+        for (var j = 1; j < tgtLength + 1; j++) {
+            realCost = src.charAt(i - 1) === tgt.charAt(j - 1) ? 0 : 1;
+            resultMatrix[i][j] = Math.min(
+                resultMatrix[i - 1][j] + 1,
+                resultMatrix[i][j - 1] + 1,
+                resultMatrix[i - 1][j - 1] + realCost // same logic as our previous example.
+            );
+        }
+    }
+
+    return resultMatrix[srcLength][tgtLength];
+}
+
 
