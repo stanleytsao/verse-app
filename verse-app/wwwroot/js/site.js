90e334cd95a15435443c7d57dcb60d309465cf58
Stanley Tsao <stsao002@gmail.com>
2018-05-10 13:59:09 -0400

    Config dropdown menus

--- verse-app/wwwroot/js/site.js
+++ verse-app/wwwroot/js/site.js
@@ -1,47 +1,64 @@
-﻿
+﻿var books = { Matthew: 28, Mark: 16, Luke: 24, John: 21, Acts: 28, Romans: 16, "1 Corinthians": 16, "2 Corinthians": 13, Galatians: 6, Ephesians: 6, Philippians: 4, Colossians: 4, "1 Thessalonians": 5, "2 Thessalonians": 3, "1 Timothy": 6, "2 Timothy": 4, Titus: 3, Philemon: 1, Hebrews: 13, James: 5, "1 Peter": 5, "2 Peter": 3, "1 John": 5, "2 John": 1, "3 John": 1, Jude: 1, Revelation: 22 };
 
 var Timer = null;
 var Time = 0;
-var Score = 0;
-var Page = 1;
 
-var Phil = ["1:6", "2:1", "2:5", "2:6", "2:12", "3:20", "4:4", "4:6-7", "4:8", "4:13", "4:19"];
-var ChVs = [30, 30, 21, 23];
-var Refs = [];
-
-$(document).ready(function () {
-    $("#TimeScore").hide();
-    $("#Typer").hide();
+$(document).ready(function() {
+    $("#PassageTyper").hide();
     $("#EndPage").hide();
     $("#History").hide();
 
+    $.each(books, function (book) {
+        $("#Book").append('<li><a href="#">' + book + '</a></li>');
+    });
 
-
-    //for (var i = 0; i < ChVs.length; i++) {
-    //    for (var j = 0; j < ChVs[i]; j++) {
-    //        Refs.push(i + 1 + ":" + j + 1);
-    //    }
-    //}
+    $("#Book li > a").click(function() {
+        var value = $(this).text();
+        $("#SelectedBook").text(value);
+        var chapters = books[value];
+        $("#Chapter").html('');
+        for (var i = 0; i < chapters; i++) {
+            $("#Chapter").append('<li><a href="#">' + (i + 1) + '</a></li>');
+            $("#Chapter li > a").click(function () {
+                var value = $(this).text();
+                $("#SelectedChapter").text(value);
+            });
+        }
+    })
 });
 
 $("#Start").click(function () {
-    $("#StartPage").hide();
-    Status = 2;
-    GetVerse();
-    $("#Typed").focus();
-    setTimeout(
-        Timer = setInterval(TimeTick, 1000),
-        1000
-    );
+    var ref = $("#SelectedBook").text() + $("#SelectedChapter").text();
+    $("#PassageTyper").show();
+    GetPassage(ref);
 });
 
-$(document).keypress(function (e) {
-    if (e.keyCode === 13 && Status === 1) {
-        $("#Start").click();
-    } else if (e.keyCode === 13 && Status === 4) {
-        $("#Restart").click();
+// ESV API
+function GetPassage(ref) {
+    $.ajax({
+        url: "https://api.esv.org/v3/passage/html/?q=" + ref + "&include-verse-anchors=false&include-chapter-numbers=false&include-first-verse-numbers=false&include-footnotes=false&include-headings=false&include-subheadings=false&include-audio-link=false",
+        type: 'GET',
+        headers: { "Authorization": "Token 00706264fcd4c12e299878dbdf3af0608adbabff" },
+        success: function (data) {
+            $("#Passage").html(data.passages);
+            $("#TextBox").focus();
+            setTimeout(
+                Timer = setInterval(TimeTick, 1000),
+                500
+            );
+        }
+    });
+}
+
+function TimeTick() {
+    Time += 1;
+    if (Time % 60 < 10) {
+        $("#Time").text("Time: " + Math.floor(Time / 60) + ":0" + Time % 60);
+    } else {
+        $("#Time").text("Time: " + Math.floor(Time / 60) + ":" + Time % 60);
     }
-});
+}
+
 
 $("#Restart").click(function () {
     $("#EndPage").hide();
@@ -72,23 +89,6 @@ $("#Typed").on("keypress", function (e) {
     }
 });
 
-function GetVerse() {
-    var ref = Phil[Math.round(Math.random() * (Phil.length - 1))];
-
-    if (Status === 3) {
-        ref = Refs[Math.round(Math.random() * (Refs.length - 1))];
-    }
-    
-    $.ajax({
-        url: "https://api.esv.org/v3/passage/html/?q=Philippians" + ref + "&include-verse-anchors=false&include-chapter-numbers=false&include-first-verse-numbers=false&include-footnotes=false&include-headings=false&include-subheadings=false&include-audio-link=false",
-        type: 'GET',
-        headers: { "Authorization": "Token 00706264fcd4c12e299878dbdf3af0608adbabff" },
-        success: function (data) {
-            $("#Verse").html(data.passages);
-        }
-    });
-}
-
 function SubmitVerse() {
     var src = $("#Typed").val().replace(/[^A-Za-z]/gi, '');
     var tgt = $("#Verse").find("p:first").text().replace(/[^A-Za-z]/gi, '');
@@ -103,24 +103,6 @@ function SubmitVerse() {
     $("#Score").text("Score: " + Score);
 
     $("#History > tbody").prepend('<tr><td>' + subscore + '</td > <td>' + percent + '%</td> <td>' + $("#Typed").val() + '</td></tr > ');
-}
-
-function TimeTick() {
-    Time -= 1;
-    if (Time === 0) {
-        SubmitVerse();
-        $("#Typed").blur();
-        clearInterval(Timer);
-        Status = 4;
-        $("#FinalScore").text("Score: " + Score);
-        $("#EndPage").show();
-    } else {
-        if (Time % 60 < 10) {
-            $("#Time").text("Time: " + Math.floor(Time / 60) + ":0" + Time % 60);
-        } else {
-            $("#Time").text("Time: " + Math.floor(Time / 60) + ":" + Time % 60);
-        }        
-    }
 }
 
 function calculateLevDistance(src, tgt) {
@@ -159,15 +141,3 @@ function calculateLevDistance(src, tgt) {
 }
 
 
-// ESV API
-function GetPassage() {
-    var ref = $("Book").val() + $("Chapter").val();
-    $.ajax({
-        url: "https://api.esv.org/v3/passage/html/?q=" + ref + "&include-verse-anchors=false&include-chapter-numbers=false&include-first-verse-numbers=false&include-footnotes=false&include-headings=false&include-subheadings=false&include-audio-link=false",
-        type: 'GET',
-        headers: { "Authorization": "Token 00706264fcd4c12e299878dbdf3af0608adbabff" },
-        success: function (data) {
-            $("#Passage").html(data.passages);
-        }
-    });
-}
