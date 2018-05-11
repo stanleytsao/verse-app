f24d88be3d095b646baa229cd48c46092b1283e0
Stanley Tsao <stsao002@gmail.com>
2018-05-11 13:08:59 -0400

    Functions for calculating WPM and Accuracy

--- verse-app/wwwroot/js/site.js
+++ verse-app/wwwroot/js/site.js
@@ -1,18 +1,22 @@
 ﻿var books = { Matthew: 28, Mark: 16, Luke: 24, John: 21, Acts: 28, Romans: 16, "1 Corinthians": 16, "2 Corinthians": 13, Galatians: 6, Ephesians: 6, Philippians: 4, Colossians: 4, "1 Thessalonians": 5, "2 Thessalonians": 3, "1 Timothy": 6, "2 Timothy": 4, Titus: 3, Philemon: 1, Hebrews: 13, James: 5, "1 Peter": 5, "2 Peter": 3, "1 John": 5, "2 John": 1, "3 John": 1, Jude: 1, Revelation: 22 };
 
 var Timer = null;
-var Time = 0;
+var Time;
+var SplitTime;
+var TotalWords;
+var FirstVerse;
+var LastVerse;
 
 $(document).ready(function() {
-    $("#PassageTyper").hide();
     $("#EndPage").hide();
-    $("#History").hide();
+    $("#PassageTyper").show();
 
+    // Generate list of books
     $.each(books, function (book) {
         $("#Book").append('<li><a href="#">' + book + '</a></li>');
     });
 
-    $("#Book li > a").click(function() {
+    $("#Book li > a").click(function () {
         var value = $(this).text();
         $("#SelectedBook").text(value);
         var chapters = books[value];
@@ -24,28 +28,43 @@ $(document).ready(function() {
                 $("#SelectedChapter").text(value);
             });
         }
-    })
+    });
 });
 
 $("#Start").click(function () {
+    Time = 0;
+    SplitTime = 0;
+    TotalWords = 0;
     var ref = $("#SelectedBook").text() + $("#SelectedChapter").text();
-    $("#PassageTyper").show();
-    GetPassage(ref);
+    GetStartEnd(ref);
 });
 
 // ESV API
-function GetPassage(ref) {
+function GetStartEnd(ref) {
+    $.ajax({
+        url: "https://api.esv.org/v3/passage/html/?q=" + ref + "&include-verse-anchors=false&include-chapter-numbers=false&include-first-verse-numbers=false&include-footnotes=false&include-headings=false&include-subheadings=false&include-audio-link=false",
+        type: 'GET',
+        headers: { "Authorization": "Token 00706264fcd4c12e299878dbdf3af0608adbabff" },
+        success: function (data) {
+            FirstVerse = data.parsed[0][0];
+            LastVerse = data.parsed[0][1];
+            GetVerse(FirstVerse);
+            $("#TextBox").focus();
+            setTimeout(
+                Timer = setInterval(TimeTick, 1000),
+                500
+            );
+        }
+    });
+}
+
+function GetVerse(ref) {
     $.ajax({
         url: "https://api.esv.org/v3/passage/html/?q=" + ref + "&include-verse-anchors=false&include-chapter-numbers=false&include-first-verse-numbers=false&include-footnotes=false&include-headings=false&include-subheadings=false&include-audio-link=false",
         type: 'GET',
         headers: { "Authorization": "Token 00706264fcd4c12e299878dbdf3af0608adbabff" },
         success: function (data) {
             $("#Passage").html(data.passages);
-            $("#TextBox").focus();
-            setTimeout(
-                Timer = setInterval(TimeTick, 1000),
-                500
-            );
         }
     });
 }
@@ -59,59 +78,42 @@ function TimeTick() {
     }
 }
 
-
-$("#Restart").click(function () {
-    $("#EndPage").hide();
-    Status = 2;
-    Score = 0;
-    GetVerse();
-    $("#Typed").focus();
-    setTimeout(
-        Timer = setInterval(TimeTick, 1000),
-        1000
-    );
-});
-
 $("#Submit").click(function () {
-    if (Status === 2) {
-        Status = 3;
+    var src = $("#TextBox").val().replace(/[^A-Za-z]/gi, '');
+    var tgt = $("#Passage").find("p:first").text().replace(/[^A-Za-z]/gi, '');
+    var max = calculateLevDistance("", tgt);
+    var act = calculateLevDistance(src, tgt);
+    var percent = ((1 - act / max) * 100).toFixed(2);
+    var words = $("#TextBox").val().split(" ").length;
+    var WPM = (words / (Time - SplitTime) * 60).toFixed(2);
+        
+    $("#History > tbody").prepend('<tr><td>' + WPM + '</td > <td>' + percent + '%</td> <td class="mdl-data-table__cell--non-numeric">' + $("#TextBox").val() + '</td></tr > ');
+    TotalWords += words;
+
+    if (FirstVerse < LastVerse) {
+        FirstVerse += 1;
+        GetVerse(FirstVerse);
+        SplitTime = Time;
     } else {
-        Status = 2;
+        clearInterval(Timer);
+        $("#FinalScore").text(TotalWords / Time);
+        $("#EndPage").show();
+        $("#PassageTyper").hide();
     }
-    SubmitVerse();
-    GetVerse();
-    $("#Typed").val('');
 });
 
 $("#Typed").on("keypress", function (e) {
-    if (e.keyCode === 13 && $("#Typed").val().length > 5) {
+    if (e.keyCode === 13 && $("#Typed").val().length > 1) {
         $("#Submit").click();    
     }
 });
 
-function SubmitVerse() {
-    var src = $("#Typed").val().replace(/[^A-Za-z]/gi, '');
-    var tgt = $("#Verse").find("p:first").text().replace(/[^A-Za-z]/gi, '');
-    console.log(src, tgt);
-    var max = calculateLevDistance("", tgt);
-    var act = calculateLevDistance(src, tgt);
-    var percent = Math.round((1 - act / max) * 100);
-    var subscore = Math.round(percent * percent * max / 1000);
-    console.log(act, max);
-
-    Score += subscore;
-    $("#Score").text("Score: " + Score);
-
-    $("#History > tbody").prepend('<tr><td>' + subscore + '</td > <td>' + percent + '%</td> <td>' + $("#Typed").val() + '</td></tr > ');
-}
 
 function calculateLevDistance(src, tgt) {
     var realCost;
-
     var srcLength = src.length,
         tgtLength = tgt.length,
         tempString, tempLength;
-
     var resultMatrix = new Array();
     resultMatrix[0] = new Array();
     
@@ -136,7 +138,6 @@ function calculateLevDistance(src, tgt) {
             );
         }
     }
-
     return resultMatrix[srcLength][tgtLength];
 }
 
